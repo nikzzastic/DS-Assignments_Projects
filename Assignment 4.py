5183e10b9a0f942e0bf4ad412ecfd49a3d8711bb
Nikhil <33477525+nikzzastic@users.noreply.github.com>
2019-07-13 13:14:47 +0530

    Create Assignment 4.py

---
+++ Assignment 4.py
@@ -0,0 +1,2 @@
+#In this project explore wine dataset to assess red wine quality. The objective of this data science project is to explore which 
+chemical properties will influence the quality of red wines.
